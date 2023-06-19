6b7da499ab31adf7b77efbff900085c51607078b
Jiten Kumar Mohanty <jitenmohantyaz@gmail.com>
2023-06-19 10:18:35 +0530

    Project Ready on Api call

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,22 @@
+import { useEffect } from 'react';
+import { getPosts } from '../api';
 
 function App() {
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const response = await getPosts();
+      console.log('response', response);
+    };
+
+    fetchPosts();
+  }, []);
+
   return (
     <div className="App">
-          <h1>Hello Geeks</h1>
+      <h1>Hello world</h1>
     </div>
   );
 }
 
 export default App;
+
